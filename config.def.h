0ae788c4c2e3b42513fca2c7f16b1f018473ed0a
Sayan Ghosh <sgic2001@gmail.com>
2020-12-06 17:58:46 +0530

    Set konsole as terminal

--- config.def.h
+++ config.def.h
@@ -49,9 +49,9 @@ static const int resizehints = 1;// 1 = respect size hints in tiled resizals
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },  /* first entry is default */
-	{ "><>",      NULL },  /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "TILED",      tile },  /* first entry is default */
+	{ "FLOAT",      NULL },  /* no layout function means floating behavior */
+	{ "MONO",      monocle },
 };
 
 /* key definitions */
@@ -68,11 +68,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "konsole", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
