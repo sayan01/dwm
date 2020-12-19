823ffe27ae1cf34bccd15535c476ea3a6cd148c9
Sayan Ghosh <sgic2001@gmail.com>
2020-12-19 17:29:53 +0530

    Add symbol to tag3

--- config.def.h
+++ config.def.h
@@ -36,7 +36,7 @@ static const char *colors[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "", "", "3", "", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
