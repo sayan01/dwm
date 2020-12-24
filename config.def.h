176c058e7d410a0c99e187a86d9895217eb625ac
Sayan Ghosh <sgic2001@gmail.com>
2020-12-24 10:04:32 +0530

    Add patch - xresources

--- config.def.h
+++ config.def.h
@@ -1,40 +1,39 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 4;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;     /* 0 means no bar */
-static const int topbar             = 1;     /* 0 means bottom bar */
-static const int user_bh            = 28;    /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static unsigned int borderpx  = 4;        /* border pixel of windows */
+static unsigned int gappx     = 8;        /* gaps between windows */
+static unsigned int snap      = 32;       /* snap pixel */
+static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static unsigned int systrayspacing = 4;   /* systray spacing */
+static int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static int showsystray        = 1;     /* 0 means no systray */
+static int showbar            = 1;     /* 0 means no bar */
+static int topbar             = 1;     /* 0 means bottom bar */
+static int user_bh            = 28;    /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = {
 	"Bitstream Vera Sans:size=12",
 	"fontawesome:size=12",
 	"Noto Color Emoji:size=12",
 };
-static const char dmenufont[]       = "Bitstream Vera Sans:size=12";
+static char dmenufont[]       = "Bitstream Vera Sans:size=12";
 // background color
-static const char col_gray1[]       = "#000000";
+static char normbgcolor[]       = "#000000";
 // inactive window border color
-static const char col_gray2[]       = "#dddddd";
+static char normbordercolor[]       = "#dddddd";
 // font color
-static const char col_gray3[]       = "#bbbbbb";
+static char normfgcolor[]       = "#bbbbbb";
 // current tag and current window font color (font on cyan)
-static const char col_gray4[]       = "#000000";
+static char selfgcolor[]       = "#000000";
 // top bar second color (blue) and active window border color (cyan)
-static const char col_cyan[]        = "#21c353";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static char selbordercolor[]        = "#21c353";
+static char selbgcolor[]        = "#21c353";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
-
-
 /* tagging */
 static const char *tags[] = { "", "", "", "", "5", "6", "7", "8", "9" };
 
@@ -66,9 +65,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.55; // factor of master area size [0.05..0.95] 
-static const int nmaster = 1;    /* number of clients in master area */
-static const int resizehints = 0;// 1 = respect size hints in tiled resizals
+static float mfact = 0.5; // factor of master area size [0.05..0.95] 
+static int nmaster = 1;    /* number of clients in master area */
+static int resizehints = 0;// 1 = respect size hints in tiled resizals
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -90,17 +89,42 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "konsole", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
-#include "selfrestart.c"
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "normbgcolor",        STRING,  &normbgcolor },
+	{ "normbordercolor",    STRING,  &normbordercolor },
+	{ "normfgcolor",        STRING,  &normfgcolor },
+	{ "selbgcolor",         STRING,  &selbgcolor },
+	{ "selbordercolor",     STRING,  &selbordercolor },
+	{ "selfgcolor",         STRING,  &selfgcolor },
+	{ "borderpx",          	INTEGER, &borderpx },
+	{ "snap",          		INTEGER, &snap },
+	{ "showbar",          	INTEGER, &showbar },
+	{ "topbar",          	INTEGER, &topbar },
+	{ "nmaster",          	INTEGER, &nmaster },
+	{ "resizehints",       	INTEGER, &resizehints },
+	{ "mfact",      	 	FLOAT,   &mfact },
+	{ "gappx",				INTEGER, &gappx },
+	{ "swallowfloating",	INTEGER, &swallowfloating },
+	{ "systraypinning",		INTEGER, &systraypinning},
+	{ "systrayspacing",		INTEGER, &systrayspacing },
+{ "systraypinningfailfirst",INTEGER, &systraypinningfailfirst },
+	{ "showsystray",		INTEGER, &showsystray },
+	{ "user_bh",			INTEGER, &user_bh },
+};
+
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-    { MODKEY|ShiftMask,             XK_a,      self_restart,   {0} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_F1,     togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
