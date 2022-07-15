17fc4f82a3d502c1bc2ee1bba201acacc1a788e0
Sayan Ghosh <sgic2001@gmail.com>
2022-07-16 03:28:21 +0530

    Add sticky patch

--- config.def.h
+++ config.def.h
@@ -172,6 +172,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_s,      togglesticky,   {0} },
 	{ 0,                            XK_F11,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
